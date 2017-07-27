475bfadd963c4c02d5b709127f62e43c736d9f6f
Jason Sears <jsears4585@gmail.com>
2017-07-26 22:25:50 -0400

    Added multiple polygons

--- client/src/components/Container.js
+++ client/src/components/Container.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-
 import { Map, GoogleApiWrapper, Polygon } from 'google-maps-react'
 
 export class MapContainer extends Component {
@@ -9,6 +8,12 @@ export class MapContainer extends Component {
       {lat: 18.466, lng: -66.118},
       {lat: 32.321, lng: -64.757},
       {lat: 25.774, lng: -80.190}
+    ]
+    var otherTriangleCoords = [
+      {lat: 35.774, lng: -90.190},
+      {lat: 28.466, lng: -76.118},
+      {lat: 42.321, lng: -74.757},
+      {lat: 35.774, lng: -90.190}
     ]
     return (
       <Map google={this.props.google}
@@ -26,6 +31,14 @@ export class MapContainer extends Component {
           strokeOpacity={0.8}
           strokeWeight={2}
           fillColor="#0000FF"
+          fillOpacity={0.35}
+      />
+      <Polygon
+          paths={otherTriangleCoords}
+          strokeColor="#FF00FF"
+          strokeOpacity={0.8}
+          strokeWeight={2}
+          fillColor="#FF00FF"
           fillOpacity={0.35}
       />
       </Map>
